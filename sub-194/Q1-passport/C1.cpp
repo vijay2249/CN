57190b66bb3b371612a619718e53c429dda6f2a3
VIJAY 'H'CN <vijaychowdary3699@gmail.com>
2022-04-18 22:01:42 -0400

    Add files via upload

---
+++ sub-194/Q1-passport/C1.cpp
@@ -0,0 +1,42 @@
+#include<sys/socket.h>
+#include<netinet/in.h> //Internet addresses
+#include<string.h> // memset
+#include<arpa/inet.h>//inet_addr
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include<sys/wait.h>
+
+#define PORT 8080
+int main(){
+    int sfd;
+    sfd=socket(AF_INET,SOCK_STREAM,0);
+    if(sfd==-1){
+        perror("Socket creation failed");
+        exit(0);
+    }
+    struct sockaddr_in addr;
+
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(PORT);
+    addr.sin_addr.s_addr= inet_addr("127.0.0.1");
+   
+
+    if(connect(sfd,(struct sockaddr *) &addr,sizeof(addr))==-1){
+        perror("Connection Failed !");
+        exit(0);
+    }
+
+   char buffer[1024]; 
+
+    memset(buffer,'\0',sizeof(buffer));
+    if(recv(sfd,buffer,sizeof(buffer),0)==-1){
+        perror("Message not recieved");
+        exit(0);
+    } 
+	printf("message recieved by A via S:%s",buffer);
+
+     close(sfd);
+
+}
